4842b0a2688e1b39b6b6e404db06e1bb62f0f119
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-02-22 11:40:03 +0100

    Delete 'beschrijving:string' and get/set from 'Inschrijving'

--- src/domein/Inschrijving.java
+++ src/domein/Inschrijving.java
@@ -10,16 +10,15 @@ package domein;
  * @author Tim
  */
 public class Inschrijving {
+
     private Formule formule;
     private Lid lid;
     private String tijdstip;
-    private String beschrijving;
 
-    public Inschrijving(Formule formule, Lid lid, String tijdstip, String beschrijving) {
+    public Inschrijving(Formule formule, Lid lid, String tijdstip) {
         setFormule(formule);
         setLid(lid);
         setTijdstip(tijdstip);
-        setBeschrijving(beschrijving);
     }
 
     public Formule getFormule() {
@@ -29,10 +28,12 @@ public class Inschrijving {
     private void setFormule(Formule formule) {
         this.formule = formule;
     }
-    public Lid getLid(){
+
+    public Lid getLid() {
         return lid;
     }
-    private void setLid(Lid lid){
+
+    private void setLid(Lid lid) {
         this.lid = lid;
     }
 
@@ -43,14 +44,6 @@ public class Inschrijving {
     private void setTijdstip(String tijdstip) {
         this.tijdstip = tijdstip;
     }
-
-    public String getBeschrijving() {
-        return beschrijving;
-    }
-
-    private void setBeschrijving(String beschrijving) {
-        this.beschrijving = beschrijving;
-    }
     
-    
+
 }
