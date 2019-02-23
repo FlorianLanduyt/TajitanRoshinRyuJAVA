34574a8bb2b7131c3a72509d684a69335b80f58b
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-02-23 14:17:13 +0100

    Removed unused import in Formule class

--- src/domein/Formule.java
+++ src/domein/Formule.java
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domein;
 
-import com.sun.corba.se.spi.orbutil.threadpool.Work;
-
-/**
- *
- * @author Tim
- */
 public enum Formule {
     DI_DO,
     DI_ZA,
